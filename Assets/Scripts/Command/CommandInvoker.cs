6072b3ebd8d768e6b3f504f6303d128b3bbd098d
Vastav <vastavpansuriya4444@gmail.com>
2024-03-02 13:26:01 +0530

    Create Command Invoker

---
+++ Assets/Scripts/Command/CommandInvoker.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+public class CommandInvoker
+{
+    private Stack<ICommand> commandRegistry = new Stack<ICommand>();
+
+    public void ProcessCommand(ICommand commandToProcess)
+    {
+        ExecuteCommand(commandToProcess);
+        RegisterCommand(commandToProcess);
+    }
+
+    public void ExecuteCommand(ICommand commandToExecute) => commandToExecute.Execute();
+
+    public void RegisterCommand(ICommand commandToRegister) => commandRegistry.Push(commandToRegister);
+}
