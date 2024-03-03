23b55e56a8669e75d42e12fe17959803331143ec
Vastav <vastavpansuriya4444@gmail.com>
2024-03-03 10:11:01 +0530

    Add Undo() Functionality in Commands

--- Assets/Scripts/Command/ConcreteCommands/AttackStanceCommand.cs
+++ Assets/Scripts/Command/ConcreteCommands/AttackStanceCommand.cs
@@ -14,6 +14,11 @@ namespace Command.Commands
 
         public override void Execute() => GameService.Instance.ActionService.GetActionByType(CommandType.AttackStance).PerformAction(actorUnit, targetUnit, willHitTarget);
 
+        public override void Undo()
+        {
+            throw new System.NotImplementedException();
+        }
+
         public override bool WillHitTarget() => true;
     }
 }
--- Assets/Scripts/Command/ConcreteCommands/MeditateCommand.cs
+++ Assets/Scripts/Command/ConcreteCommands/MeditateCommand.cs
@@ -14,6 +14,11 @@ namespace Command.Commands
 
         public override void Execute() => GameService.Instance.ActionService.GetActionByType(CommandType.Meditate).PerformAction(actorUnit, targetUnit, willHitTarget);
 
+        public override void Undo()
+        {
+            throw new System.NotImplementedException();
+        }
+
         public override bool WillHitTarget() => true;
     }
 }
